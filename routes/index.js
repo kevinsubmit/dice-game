f1d2ebd442b01454e1ad65080ba76facd4632414
austin li <645267294@qq.com>
2024-11-03 16:38:20 -0500

    fix some bugs again

--- routes/index.js
+++ routes/index.js
@@ -3,18 +3,18 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'dice game' });
 });
 
 router.get('/register', function(req, res, next) {
   // res.send('respond with a resource');
-  res.render('register', { title: 'Express' })
+  res.render('register', { title: 'dice game' })
 
 
 });
 router.get('/login', function(req, res, next) {
   // res.send('respond with a resource');
-  res.render('login', { title: 'Express' })
+  res.render('login', { title: 'dice game' })
 
 
 });
