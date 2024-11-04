8ab300a4d052f4476953543e8709c008844833e3
austin li <645267294@qq.com>
2024-11-03 20:09:51 -0500

    fix some bugs againn

--- public/javascripts/rules.js
+++ public/javascripts/rules.js
@@ -1,13 +1,13 @@
 // 规则弹出框控制
 document.addEventListener('DOMContentLoaded', function() {
-  const rulesBtn = document.getElementById('rules-btn');
+  // const rulesBtn = document.getElementById('rules-btn');
   const rulesModal = document.getElementById('rules-modal');
   const closeRules = document.querySelector('.close-rules');
 
   // 打开规则
-  rulesBtn.addEventListener('click', () => {
-    rulesModal.style.display = 'block';
-  });
+  // rulesBtn.addEventListener('click', () => {
+  //   rulesModal.style.display = 'block';
+  // });
 
   // 关闭规则
   closeRules.addEventListener('click', () => {
