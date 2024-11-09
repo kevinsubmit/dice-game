5659cc7cf84636931d784fcdcd7a75a7b2088d48
austin li <645267294@qq.com>
2024-11-08 21:17:07 -0500

    updated folder structure

--- api/login.js
+++ api/login.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const User = require("../models/User");
+const User = require("./models/User");
 
 // 注册路由  注册这部分是没问题的
 router.post("/register", async (req, res) => {
